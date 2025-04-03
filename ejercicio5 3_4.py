5e0ea9c38edf08b29b0de7eb56493232758ba712
paulacersofios <pcersofios@gmail.com>
2025-04-03 19:21:29 -0300

    Add files via upload

---
+++ ejercicio5 3_4.py
@@ -0,0 +1,17 @@
+frutas = ["Manzana", "Pera", "Uva", "Mandarina"]
+frutas.append("banana")
+print(frutas[1])
+
+frutas.insert(2, "durazno")
+frutas.remove("Uva")
+frutas.sort()
+frutas.reverse()
+for fruta in frutas:
+    print(fruta)
+    
+
+
+contador=0
+while contador == 5:
+    print(contador)
+    contador +=1
