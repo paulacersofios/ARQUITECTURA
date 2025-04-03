b6edd037cb73594321f102e97e1c2aa3d80381df
paulacersofios <pcersofios@gmail.com>
2025-04-03 18:37:30 -0300

    Add files via upload

---
+++ ejercicio2. 3_4 .py
@@ -0,0 +1,9 @@
+v1 = input("Ingrese valor 1:")
+v2 = input("ingrese valor 2:")
+print (v1,v2)
+if v1 == v2:
+    print("Son iguales")
+elif v1 > v2:
+    print("V1 es mayor que v2")
+elif v2 > V1:
+    print("v2 es mayor que V1")
