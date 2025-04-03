5e51a4d35d6e31a38e8b24b62539a13af61aa8c9
paulacersofios <pcersofios@gmail.com>
2025-04-03 18:38:25 -0300

    Add files via upload

---
+++ ejercicio3. 3_4.py
@@ -0,0 +1,8 @@
+if clasificación >= 90:
+    print ("Excelente")
+elif clasificación >= 80:
+    print ("Muy bueno")
+elif clasificación >= 70:
+    print ("Bueno")
+else clasificación < 70:
+   print ("Necesita mejorar.")
