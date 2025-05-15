04fb7703e67f25dcf9d5f979f348acf9ca02f136
paulacersofios <pcersofios@gmail.com>
2025-05-15 19:40:56 -0300

    Update ejercicio5 3_4.py

--- ejercicio5 3_4.py
+++ ejercicio5 3_4.py
@@ -23,3 +23,11 @@ while contador <= 10:
     
     contador +=1
     print(contador)
+
+
+suma=0
+valor=int(input("ingrese un valor:"))
+while valor !=0:
+    suma=suma+valor
+    valor=int(input("inserte un valor"))
+print(f"el total es {suma}")
